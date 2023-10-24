b501fcecb10cc71845a7c99e602fa5e982316400
unknown <minsoojo5@gmail.com>
2023-10-24 16:17:29 -0400

    kinda fixed hero images

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,20 +1,21 @@
-import React from "react";
+const Pet = ({ name, animal, breed, images, location, id }) => {
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+  if (images && images.length) {
+    hero = images[0];
+  }
 
-// const Pet = (props) => {
-//     return React.createElement("div", {}, [
-//       React.createElement("h1", {}, props.name),
-//       React.createElement("h2", {}, props.animal),
-//       React.createElement("h2", {}, props.breed),
-//     ]);
-//   };
-
-const Pet = (props) => {
   return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
-      <h3>{props.breed}</h3>
-    </div>
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt="" />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>
+          {animal} — {breed} — {location}
+        </h2>
+      </div>
+    </a>
   );
 };
 
