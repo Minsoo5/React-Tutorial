4bd6100bbae52149b94eb33bb6759b5b158dfa84
unknown <minsoojo5@gmail.com>
2023-10-24 12:54:57 -0400

    search breeds

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = ["poodle"];
+
   return (
     <div className="search-params">
       <form>
@@ -14,6 +19,40 @@ const SearchParams = () => {
             placeholder="Location"
           />
         </label>
+
+        <lable htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </lable>
+
+        <lable htmlFor="breed">
+          Breeds
+          <select
+            id="breed"
+            disabled={breeds.length === 0}
+            value={breed}
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed}>{breed}</option>
+            ))}
+          </select>
+        </lable>
         <button>Submit</button>
       </form>
     </div>
