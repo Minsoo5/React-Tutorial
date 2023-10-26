1ff144242d0e8506f70c0867bc346ab555a137b6
unknown <minsoojo5@gmail.com>
2023-10-26 10:36:22 -0400

    Query Client Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
 import { createRoot } from "react-dom/client";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParams from "./SearchParams";
 import Details from "./Details.jsx";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity, // Time scale done in milli
+      cacheTime: Infinity, // Keep the cache per session. No need to update during one.
+    },
+  },
+});
+
+// BrowserRouter and QueryProvides are higher order components. They wrap other components but they themselves do not display anything
+// They provide context to the components underneath it.
 const App = () => {
+  // Displays Adopt Me and SearchParams on "/" which is default
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/">Adopt Me!</Link>
-      </header>
-      <Routes>
-        <Route path="/details/:id" element={<Details />} />
-        <Route path="/" element={<SearchParams />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
