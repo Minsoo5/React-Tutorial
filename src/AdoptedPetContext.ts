3a5c61d4437253bcf43ec8cee58ce81b6073a3a2
unknown <minsoojo5@gmail.com>
2023-11-01 09:10:01 -0400

    starting adoptedpet conversion

--- src/AdoptedPetContext.ts
+++ src/AdoptedPetContext.ts
@@ -1,6 +1,18 @@
 import { createContext } from "react";
+import { Pet } from "./APIResponsesTypes";
 
-const AdoptedPetContext = createContext(); // Could put in default value in createContext()
+const AdoptedPetContext = createContext<[Pet, (adoptedPet: Pet) => void]>([
+  {
+    id: 1337,
+    name: "Fido",
+    animal: "dog",
+    description: "Lorem ipsum",
+    breed: "Beagle",
+    images: [],
+    city: "Seattle",
+  },
+  () => {},
+]); // Could put in default value in createContext()
 
 console.log(`From inside AdoptedPetContext: ${AdoptedPetContext}`);
 
